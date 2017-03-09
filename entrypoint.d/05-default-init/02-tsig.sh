b1549a1d839c6e6399aad879a30bc72e01d82f84
Diego Abelenda <diego.abelenda@camptocamp.com>
2017-03-09 09:27:55 +0100

    Missed one possible char in regex

--- entrypoint.d/05-default-init/02-tsig.sh
+++ entrypoint.d/05-default-init/02-tsig.sh
@@ -2,10 +2,10 @@
 
 if [ ! -z "${BIND_KEYS_LIST}" ]; then
   while read -r line; do
-    if echo $line | grep -vq '^[[:space:]]*\([-[:alnum:]\._=]\+\)[[:space:]]\+\([[:alnum:]/=]\+\)[[:space:]]*$'; then
+    if echo $line | grep -vq '^[[:space:]]*\([-[:alnum:]\._=]\+\)[[:space:]]\+\([[:alnum:]/=+]\+\)[[:space:]]*$'; then
       echo "'$line' is malformed in BIND_KEYS_LIST (ignoring)" > /dev/stderr
     fi
 
-    echo $line | sed -n s/'^[[:space:]]*\([-[:alnum:]\._=]\+\)[[:space:]]\+\([[:alnum:]/=]\+\)[[:space:]]*$'/'key \1 { algorithm hmac-md5; secret "\2"; };'/p >> /etc/named.conf
+    echo $line | sed -n s/'^[[:space:]]*\([-[:alnum:]\._=]\+\)[[:space:]]\+\([[:alnum:]/=+]\+\)[[:space:]]*$'/'key \1 { algorithm hmac-md5; secret "\2"; };'/p >> /etc/named.conf
   done <<< "${BIND_KEYS_LIST}"
 fi
